0854018293572d59869e6dcbb2c991eb46d262fb
renen <renen@ubuntu.(none)>
2013-05-07 19:56:20 +0000

    added precipitation forecast based on yr.no for the next 24, 48 and 72 hours

---
+++ scripts/weather_yr_no.rb
@@ -0,0 +1,115 @@
+require 'nokogiri'
+require 'open-uri'
+require 'socket'
+require 'log_wrapper'
+require 'json'
+require_relative '../config/config'
+
+@log   	= Log_Wrapper.new( File.join(File.dirname(__FILE__), 'log.txt') )
+
+
+FORECAST_URL = 'http://api.yr.no/weatherapi/locationforecast/1.8/?lat=-33.95283;lon=18.48056'
+
+# resultant xml includes three types of node:
+# precipitation forecasts, containing only a precipitation element (node) with a three hour window. Three hourly, for the next three days.
+# precipitation forecasts, containing only a precipitation element (node) with a six hour window. Three hourly for the first three days, and thereafter, six hourly for the next week or so.
+# general forecast nodes, containing wind and all that. Six hourly, for the next week or so.
+
+# sigh. or something like that.
+
+def get_value(type, detail)
+	case type
+	when 'temperature', 'humidity', 'pressure', 'precipitation'
+		r = detail['value']
+	when 'winddirection'
+		r = detail['name']
+	when 'windspeed'
+		r = detail['mps']
+	when 'cloudiness', 'fog', 'lowclouds', 'mediumclouds', 'highclouds'
+		r = detail['percent']
+	else
+		nil
+	end
+end
+
+def get_element(period_forecast)
+	# skip over the location node
+	r = {}
+	period_forecast.children[0].children.each do |detail|
+		value = get_value(detail.name, detail)
+		r[detail.name] = value if value
+	end	
+	r
+end
+
+def retrieve_forecast()
+	forecast = {}
+	doc = Nokogiri::HTML(open(FORECAST_URL))
+	doc.xpath('//time').each do |period_forecast|
+		key = "#{period_forecast['from']} #{period_forecast['to']}"
+		start 			= Time.parse(period_forecast['from']).to_i
+		stop  			= Time.parse(period_forecast['to']).to_i
+		duration		= (stop-start)/3600
+		duration		= 6 if duration == 0
+		forecast[key] ||= {
+			'from' 			=> period_forecast['from'],
+			'to'	 			=> period_forecast['to'],
+			'duration'	=> duration,
+			'start'			=> start,
+		}
+		forecast[key].merge!(get_element(period_forecast))
+	end
+	forecast
+end
+
+def publish(type, message)
+	#p "#{type} #{message.to_json}"
+	socket = UDPSocket.new
+	socket.send("#{type} #{message.to_json}", 0, UMMP_IP, UMMP_PORT)
+end
+
+def get_precipitation_forecast
+	# for now, I am only interested in the forecast level of precipitation over the next 24, 48 and 72 hours.
+	forecast 	= retrieve_forecast
+
+	precipitation_forecast = []
+	rainfall_forecast			 = [0.0,0.0,0.0]
+
+	first = Time.now.to_i + 24*60*60*5
+	forecast.each_value do |f|
+		if f['precipitation'] && f['duration']==3
+			precipitation_forecast << f
+			first = f['start'] if f['start'] < first
+		end
+	end
+
+	precipitation_forecast.each do |f|
+		period = (f['start'] - first) / (24*60*60)
+		rainfall_forecast[period] += f['precipitation'].to_f if period < 3
+	end
+
+	{
+		'precipitation' => rainfall_forecast,
+		'start'					=> first
+	}
+
+end
+
+def test
+	forecast = retrieve_forecast
+	forecast.each_value do |f|
+		start = Time.parse(f['from']).to_i
+		stop  = Time.parse(f['to']).to_i
+		p "#{(f['precipitation'] ? 'p' : 'w')}#{f['duration']} #{start} #{f['from']} #{f['to']} #{(f['precipitation'] ? f['precipitation'] + "mm" : f['temperature'] + 'C')}"
+	end
+end
+
+def run
+	rainfall_forecast = nil
+	@log.debug("Retrieve precipitation forecast") do
+		rainfall_forecast = get_precipitation_forecast
+	end
+	publish('precipitation', rainfall_forecast) if rainfall_forecast
+end
+
+run

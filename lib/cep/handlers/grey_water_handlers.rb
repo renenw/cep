badea34ffbbfb0e5490a0fa8a62d861150c881f3
renen <renen@121.co.za>
2014-01-04 12:32:03 +0200

    added handler for basement sump switch

--- lib/cep/handlers/grey_water_handlers.rb
+++ lib/cep/handlers/grey_water_handlers.rb
@@ -1,18 +1,27 @@
 module Grey_Water_Handlers
 
-	def after_received_grey_water_flooded(payload)
+  def after_received_grey_water_flooded(payload)
 
-		flooded = (payload['integer_value']==1)
+    flooded = (payload['integer_value']==1)
 
-		message = "Grey water cistern flooded"
-		message = "Grey water cistern *NO LONGER* flooded" unless flooded
-		
+    message = "Grey water cistern flooded"
+    message = "Grey water cistern *NO LONGER* flooded" unless flooded
+    
     log_message 'grey_water', :error, message, payload
-    sms       	message, payload['guid']
+    sms         message, payload['guid']
     tweet       message, payload['guid']
-	
-	  payload
+  
+    payload
 
-	end
+  end
+
+  def after_received_basement_sump_flooded(payload)
+    flooded = (payload['integer_value']==1)
+    message = "Basement sump flooded"
+    message = "Basement sump *NO LONGER* flooded" unless flooded   
+    log_message 'basement_sump', :error, message, payload
+    tweet       message, payload['guid']
+    payload
+  end
 
 end

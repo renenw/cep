914e8dab6999453df157804cc77ccfefdbb2b48a
renen <renen@121.co.za>
2015-05-17 14:34:42 +0200

    used temp files; not sure how it was previously working

--- scripts/network_speed.sh
+++ scripts/network_speed.sh
@@ -7,17 +7,23 @@ size=2048000
 
 TIMEFORMAT='%6R'
 
+# Make sure things are clear
+rm -f /tmp/download_test.t1
+rm -f /tmp/download_test.t2
+rm -f /tmp/download_test.t3
+rm -f /tmp/download_test.t4
+
 # Get the file repeatedly
-t1=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t1 > /dev/null; } 2>&1 )
-t2=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t2 > /dev/null; } 2>&1 )
-t3=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t3 > /dev/null; } 2>&1 )
-t4=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t4 > /dev/null; } 2>&1 )
+t1=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t1 > /tmp/download_test.t1; } 2>&1 )
+t2=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t2 > /tmp/download_test.t2; } 2>&1 )
+t3=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t3 > /tmp/download_test.t3; } 2>&1 )
+t4=$( { time curl -s "$target" -o /home/renen/cep/scripts/download_tests/download_test.t4 > /tmp/download_test.t4; } 2>&1 )
 
 # Confirm file sizes
-s1=$(stat -c%s "download_test.t1")
-s2=$(stat -c%s "download_test.t2")
-s3=$(stat -c%s "download_test.t3")
-s4=$(stat -c%s "download_test.t4")
+s1=$(stat -c%s "/tmp/download_test.t1")
+s2=$(stat -c%s "/tmp/download_test.t2")
+s3=$(stat -c%s "/tmp/download_test.t3")
+s4=$(stat -c%s "/tmp/download_test.t4")
 
 # Make sure we got down a file
 if [ "$s1" -ne "$size" ] ; then

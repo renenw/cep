d944e43e32078245c50f5099ac4fb90140b8d4e0
renen <renen@121.co.za>
2015-05-17 13:07:56 +0200

    added logging of gate accesses

--- lib/cep/handlers/access_handlers.rb
+++ lib/cep/handlers/access_handlers.rb
@@ -5,7 +5,7 @@ module Access_Handlers
     message = "#{payload['data'].capitalize} accessed via the #{MONITORS[payload['source']][:name]}"
 
     broadcast_message_to_websockets 'access', message, payload
-    #log_message 'access', :error, message, payload
+    log_message 'access', :error, message, payload
     #tweet       message, payload['guid']
 
 	  nil

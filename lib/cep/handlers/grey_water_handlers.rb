8008bb1460263c53fb8cf5e21513d11a99ffdca0
renen <renen@121.co.za>
2015-12-28 11:31:18 +0200

    maded basement sump flooding also SMS

--- lib/cep/handlers/grey_water_handlers.rb
+++ lib/cep/handlers/grey_water_handlers.rb
@@ -16,12 +16,18 @@ module Grey_Water_Handlers
   end
 
   def after_received_basement_sump_flooded(payload)
+    
     flooded = (payload['integer_value']==1)
+    
     message = "Basement sump flooded"
-    message = "Basement sump *NO LONGER* flooded" unless flooded   
+    message = "Basement sump *NO LONGER* flooded" unless flooded
+
     log_message 'basement_sump', :error, message, payload
+    sms         message, payload['guid']
     tweet       message, payload['guid']
+
     payload
+    
   end
 
 end
